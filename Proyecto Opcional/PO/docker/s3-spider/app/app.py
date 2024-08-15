6f6cd5fee35e8f69b5937e9c2fcc84091fb810b5
Victor Aymerich Quesada <vaymerich@estudiantec.cr>
2024-08-14 22:02:27 -0600

    Final version of the spider 3 and also kronjob configuration added

--- Proyecto Opcional/PO/docker/s3-spider/app/app.py
+++ Proyecto Opcional/PO/docker/s3-spider/app/app.py
@@ -22,9 +22,7 @@ MARIADB_PASS = os.getenv('MARIADB_PASS')
 MARIADB = os.getenv('MARIADB')
 MARIADB_DB = os.getenv('MARIADB_DB')
 MARIADB_TABLE = os.getenv('MARIADB_TABLE')
-
- 
-hostname = os.getenv('HOSTNAME')
+PROCESSED_TABLE = os.getenv('PROCESSED_TABLE')
 
 credentials = pika.PlainCredentials('user', RABBIT_MQ_PASSWORD)
 parameters = pika.ConnectionParameters(host=RABBIT_MQ, credentials=credentials) 
@@ -59,6 +57,31 @@ def mariadb_connection():
 
 # Funtions
 
+# Check if an object has been processed
+def is_object_processed(file_key):
+    mariadb_conn, cursor = mariadb_connection()
+    try:
+        cursor.callproc('CheckIfObjectProcessed', [file_key])
+        result = cursor.fetchone()[0]
+    except mariadb.Error as e:
+        print(f"Error calling stored procedure CheckIfObjectProcessed: {e}")
+        result = 0
+    cursor.close()
+    mariadb_conn.close()
+    return result > 0
+
+# Mark an object as processed in the database
+def mark_object_as_processed(file_key):
+    mariadb_conn, cursor = mariadb_connection()
+    try:
+        cursor.callproc('MarkObjectAsProcessed', [file_key])
+        mariadb_conn.commit()
+    except mariadb.Error as e:
+        print(f"Error calling stored procedure MarkObjectAsProcessed: {e}")
+    finally:
+        cursor.close()
+        mariadb_conn.close()
+
 # Extract the dois from the content of the object
 def extract_dois(text):
     # Regex para identificar patrones de DOI válidos
@@ -70,6 +93,8 @@ def extract_dois(text):
     # Filter the results to ensure they are valid DOIs
     valid_dois = [doi for doi in potential_dois if re.match(doi_pattern, doi)]
 
+    valid_dois = list(set(valid_dois))
+
     return valid_dois
 
 # Function to get the content of a file in S3
@@ -79,7 +104,6 @@ def get_file_content(bucket, key):
     return content
 
 # Function to list the content of an object in a list with its Dois
-# TODO Por alguna razon crea dois nuevos cuando se compara con el contenido original esto se da en la funcion extract_dois 
 def create_jobs(data, job_size):
     # Leave only the dois
     dois = extract_dois(data)
@@ -111,45 +135,51 @@ def list_all_objects(bucket):
 
 # Function to save job information to MariaDB
 def insert_job_to_mariadb(job_id, dois):
-    table_name = MARIADB_TABLE
-    db_name = MARIADB_DB
-    
     mariadb_conn, cursor = mariadb_connection()
-    # Insert the job into the specified table
     try:
-        insert_query = f"INSERT INTO {table_name} (ID, Estado, DOIs, omitido, fecha_inicio, fecha_fin) VALUES (%s, %s, %s, %s, %s, %s)"
-        cursor.execute(insert_query, (job_id, 'pending', ','.join(dois), '', dt.datetime.now(), None))
-        print(f"Job ID {job_id} inserted into {table_name} in {db_name}")
+        cursor.callproc('InsertJob', [job_id, 'pending', ','.join(dois), '', dt.datetime.now(), None])
         mariadb_conn.commit()
+        print(f"Job ID {job_id} inserted into {MARIADB_TABLE} in {MARIADB_DB}")
         return True 
     except mariadb.Error as e:
         print(f"Error inserting job into MariaDB: {e}")
         return False
     finally:
-        cursor.close()  
-        mariadb_conn.close()  
+        cursor.close()
+        mariadb_conn.close()
 
 
 # Main functionality
 
-# Get the objects to read 
+#Get the files in the bucket
 files_to_read = list_all_objects(BUCKET)
 
+# Get the objects in the  
 for file in files_to_read:
-    content = get_file_content(BUCKET, file['Key'])
-    jobs = create_jobs(content, job_size=10)  # Adjust job_size as needed
+    if not is_object_processed(file['Key']):
+        content = get_file_content(BUCKET, file['Key'])
+        jobs = create_jobs(content, job_size=10)  # Adjust job_size as needed
 
-    for job in jobs:
-        id = str(uuid.uuid4())
-        insert = insert_job_to_mariadb(id, job)
+        for job in jobs:
+            id = str(uuid.uuid4())
+            insert = insert_job_to_mariadb(id, job)
 
-        if insert :
-            # Publish job ID to RabbitMQ
-            msg = f"Job ID: {id}"
-            channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=msg)
-            print(f"Published Job ID {id} to RabbitMQ")
-        else :
-            print(f"Error inserting job {id} to MariaDB")
+            if insert :
+                # Publish job ID to RabbitMQ
+                msg = f"Job ID: {id}"
+                channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=msg)
+                # Mark the object as processed
+                print(f"Published Job ID {id} to RabbitMQ")
+            else :
+                print(f"Error inserting job {id} to MariaDB")
+        mark_object_as_processed(file['Key'])
+    else:
+        print(f"Object {file['Key']} already processed")
+        
+
 # Clean up
 connection.close()
- 
+#exit aplication
+print("Application finished")
+sys.exit(0)
+
