f6ce028ef7dd150f4a9193488e4b46aeca891f1a
tonybarr1611 <antbarrantes@estudiantec.cr>
2024-08-25 12:45:56 -0600

    Implemented databases in the various namespaces
    Added Redis, MemCached, Elastic, PostgresSQL and MariaDB to the Helm Charts

--- Tarea Corta 1/TC1/charts/uninstall.sh
+++ Tarea Corta 1/TC1/charts/uninstall.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 helm list
-helm uninstall application
+helm uninstall grafana-config
+sleep 15
+helm uninstall app
 sleep 15
 helm uninstall databases
+sleep 40
+helm uninstall monitoring-stack
 sleep 60
 helm uninstall bootstrap
