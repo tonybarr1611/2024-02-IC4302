7522d59603c483a3c7905504f177ce0bd3d37ac6
tonybarr1611 <antbarrantes@estudiantec.cr>
2024-09-26 22:42:22 -0600

    Backend API
    Implemented the backend API, exposed it through a NodePort and Port Forwarding.

--- Proyecto 1/P1/docker/build.sh
+++ Proyecto 1/P1/docker/build.sh
@@ -2,13 +2,13 @@
 # $1 is the username
 docker login
 
-# cd backend-api
-# docker build -t $1/backend-api .
-# docker push $1/backend-api
+cd backend-api
+docker build -t $1/backend-api .
+docker push $1/backend-api
 
-# cd ../frontend
-# docker build -t $1/frontend .
-# docker push $1/frontend
+cd ../frontend
+docker build -t $1/frontend .
+docker push $1/frontend
 
 # cd ./huggingface-api
 # docker build -t $1/huggingface-api .
@@ -18,6 +18,6 @@ cd ../ingest
 docker build -t $1/ingest .
 docker push $1/ingest
 
-# cd ../s3-crawler
-# docker build -t $1/s3-crawler .
-# docker push $1/s3-crawler
+cd ../s3-crawler
+docker build -t $1/s3-crawler .
+docker push $1/s3-crawler
