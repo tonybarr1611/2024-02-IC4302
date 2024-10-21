92e7db9223bbc23fcf92bee12648479537d133c4
Fabricio Solis <sfabricito@gmail.com>
2024-10-21 15:33:07 -0600

    feat: basic structure of loader component

--- Proyecto 2/P2/docker/loader/app/app.py
+++ Proyecto 2/P2/docker/loader/app/app.py
@@ -1,1 +1,36 @@
-print("Loader")
+import csv
+from google.cloud import storage
+
+SERVICE_ACCOUNT_CREDENTIALS = "./service_account.json"
+
+BUCKET_NAME = "ic4302-202402"
+
+def readCSVFile(blob):
+    content = blob.download_as_text()
+    return csv.reader(content.splitlines())
+
+def processArtist(file):
+    print('Processing Artist')
+
+def processLyric(file):
+    print('Processing Lyric')
+
+def readGoogleCloudBucket():
+    client = storage.Client.from_service_account_json(SERVICE_ACCOUNT_CREDENTIALS)
+    blobs = client.list_blobs(BUCKET_NAME)
+    return blobs
+
+def processBlobs(blobs):
+    print(f"Files in bucket '{BUCKET_NAME}':")
+    for blob in blobs:
+        if blob.name.endswith(".csv"):
+            print(f"Processing CSV file: {blob.name}")
+            file = readCSVFile(blob)
+            if blob.name == 'artists-data.csv':
+                processArtist(file)
+            else:
+                processLyric(file)
+
+if __name__ == "__main__":
+    blobs = readGoogleCloudBucket()
+    processBlobs(blobs)
