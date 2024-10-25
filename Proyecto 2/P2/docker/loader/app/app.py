539f9d5af8674761979f526f0dc9e4d7eee881c3
Fabricio Solis <sfabricito@gmail.com>
2024-10-25 00:35:30 -0600

    feat: Download data from Google Cloud Storage, process it and insert into PostgreSQL and MongoDB

--- Proyecto 2/P2/docker/loader/app/app.py
+++ Proyecto 2/P2/docker/loader/app/app.py
@@ -1,36 +1,72 @@
 import csv
+from os import getenv
 from google.cloud import storage
+from utils.postgres import generatePostgresConnection, initDB, insertDataPostgres, get
+from utils.mongo import generateMongoConnection, insertDataMongo
 
+BUCKET_NAME = getenv('GCS_BUCKET')
 SERVICE_ACCOUNT_CREDENTIALS = "./service_account.json"
-
-BUCKET_NAME = "ic4302-202402"
 
 def readCSVFile(blob):
     content = blob.download_as_text()
-    return csv.reader(content.splitlines())
+    reader = csv.reader(content.splitlines())
+    data = [row for row in reader if row][1:]
+    return data
 
-def processArtist(file):
-    print('Processing Artist')
+def processArtist(postgresConnection, mongoConnection, data):
+    header = ['Name', 'Genres', 'Songs', 'Popularity', 'Link']
+    insertDataPostgres(
+        postgresConnection,
+        'Artist',
+        header,
+        data
+    )
+    insertDataMongo(
+        mongoConnection,
+        'Artist',
+        header,
+        data
+    )
 
-def processLyric(file):
-    print('Processing Lyric')
+def processSong(postgresConnection, mongoConnection, data):
+    header = ['ArtistLink', 'Name', 'Link', 'Lyric','Language']
+    insertDataPostgres(
+        postgresConnection,
+        'Song',
+        header,
+        data
+    )
+    insertDataMongo(
+        mongoConnection,
+        'Song',
+        header,
+        data
+    )
 
 def readGoogleCloudBucket():
     client = storage.Client.from_service_account_json(SERVICE_ACCOUNT_CREDENTIALS)
     blobs = client.list_blobs(BUCKET_NAME)
     return blobs
 
-def processBlobs(blobs):
+def processBlobs(postgresConnection, mongoConnection, blobs):
     print(f"Files in bucket '{BUCKET_NAME}':")
     for blob in blobs:
         if blob.name.endswith(".csv"):
             print(f"Processing CSV file: {blob.name}")
             file = readCSVFile(blob)
             if blob.name == 'artists-data.csv':
-                processArtist(file)
+                processArtist(postgresConnection, mongoConnection, file)
             else:
-                processLyric(file)
+                processSong(postgresConnection, mongoConnection, file)
 
 if __name__ == "__main__":
+    # PostgreSQL
+    postgresPool = generatePostgresConnection()
+    initDB(postgresPool)
+
+    # Mongo
+    mongoPool = generateMongoConnection()
+
+    # Execution
     blobs = readGoogleCloudBucket()
-    processBlobs(blobs)
+    processBlobs(postgresPool, mongoPool, blobs)
