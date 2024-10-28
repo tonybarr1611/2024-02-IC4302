27337f6bcc33f500ef08285095bb55e566b2bcda
Fabricio Solis <sfabricito@gmail.com>
2024-10-28 17:29:20 -0600

    fix: execute a query in mongo modify to do a full text search

--- Proyecto 2/P2/docker/backendStayTune/app/utils.py
+++ Proyecto 2/P2/docker/backendStayTune/app/utils.py
@@ -24,10 +24,24 @@ def executePostgresQuery(query):
         print(f"No data found: {e}")
         return []
     
-def executeMongoQuery(collection, query):
+def executeMongoQuery(text):
     global mongodb_connection
     db = mongodb_connection[MONGO_DB]
     collection = db[collection]
-    results = collection.find(query)
+
+    # Create index search
+    sampleDocument = collection.find_one()
+    if sampleDocument:
+        fields = list(sampleDocument.keys())[1:]
+        index_spec = [(field, "text") for field in fields]
+        collection.create_index(index_spec)
+
+    results = collection.find({"$text": {"$search": text}})
 
     return results
+
+def executeQuery(database, query):
+    if database.lower() == "postgres":
+        executePostgresQuery(query)
+    elif database.lower() == "mongo":
+        executeMongoQuery(query)
